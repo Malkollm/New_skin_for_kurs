926273b6330a8da3243ca410801d0049f0dea9d2
Malkollm <fatkullin@bk.ru>
2016-06-12 09:55:36 +0300

    изменить макет для анкеты врача

---
+++ User_window/src/sample/Controller.java
@@ -0,0 +1,17 @@
+package sample;
+
+import javafx.fxml.FXML;
+
+import java.awt.event.ActionEvent;
+
+public class Controller {
+    @FXML
+    private void search (ActionEvent event){
+
+    }
+
+    @FXML
+    private void help (ActionEvent event){
+
+    }
+}
